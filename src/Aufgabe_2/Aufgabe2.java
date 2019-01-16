8b454ae60d92fd44dadb5e7f9db1c334a43b61ab
TheresaSchmotz <schmotz.theresa@gmail.com>
2019-01-16 23:56:54 +0100

    Aufgabe 2 done

--- src/Aufgabe_2/Aufgabe2.java
+++ src/Aufgabe_2/Aufgabe2.java
@@ -1,4 +1,36 @@
 package Aufgabe_2;
 
+import javax.swing.*;
+
+
 public class Aufgabe2 {
+
+    public static void main(String[] args) {
+
+        while(true){
+            String input = JOptionPane.showInputDialog("Gib mir ne Zahl, keine Buchstaben du Lärri");
+
+            if(isInteger(input)){
+                int zahl = Integer.parseInt(input);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    public static boolean isInteger(String s)
+    {
+        try
+        {
+            Integer.parseInt(s); //Anweisung, die zu Fehlern führen kann.
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println("Brot kann schimmeln, was kannst du.");
+            return false; //Anweisung im Fehlerfall NumberFormatException
+        }
+        return true;
+    }
 }
