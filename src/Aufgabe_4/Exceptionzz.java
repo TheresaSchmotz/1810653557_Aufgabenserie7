4aed51d0be9f19e2d4b0e0485dea4115df952b5e
TheresaSchmotz <schmotz.theresa@gmail.com>
2019-01-17 00:56:50 +0100

    Es sollte richtig sein aber Intellij sagt no

---
+++ src/Aufgabe_4/Exceptionzz.java
@@ -0,0 +1,10 @@
+package Aufgabe_4;
+
+public class Exceptionzz {
+
+    public static void main(String[] args) throws Ibimez {
+
+        throw new Ibimez();
+        //hier gibt es eine neue exception aus
+    }
+}
---
+++ src/Aufgabe_4/Ibimez.java
@@ -0,0 +1,11 @@
+package Aufgabe_4;
+
+public class Ibimez extends Exceptionzz {
+
+    public Ibimez()
+    {
+        super("I bims.");
+    }
+
+    //hier wird die exception gemacht
+}
